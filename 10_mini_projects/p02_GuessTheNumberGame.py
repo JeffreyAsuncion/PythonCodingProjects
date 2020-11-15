107c95cef6933ae2af79998737a04a2e89b2bcda
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-11-15 18:35:11 -0500

    p02 and p04 outlines

---
+++ 10_mini_projects/p02_GuessTheNumberGame.py
@@ -0,0 +1,24 @@
+"""
+The main goal of the project is 
+to create a program that 
+randomly select a number in a range 
+then the user has to guess the number. 
+user has three chances to guess the number 
+if he guess correct 
+then a message print saying “you guess right 
+“otherwise a negative message prints.
+
+Topics: random module, for loop, f strings
+"""
+
+
+# import random
+# number = random.randint(1,10)
+# for i in range(0,3):
+#     user = int(input("guess the number"))
+#     if user == number:
+#         print("Hurray!!")
+#         print(f"you guessed the number right it's {number}")
+#         break
+# if user != number:
+#     print(f"Your guess is incorrect the number is {number}")
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PythonCodingProjects
 Python Projects from Beginner-Intermediate 
+
+
+https://towardsdatascience.com/10-python-mini-projects-that-everyone-should-build-with-code-769c6f1af0c4
+
+
+https://medium.com/analytics-vidhya/5-python-intermediate-project-that-everyone-should-build-with-codes-5039f13feadb
+
