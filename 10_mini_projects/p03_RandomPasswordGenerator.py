0c205481f4e6200c0b6f6eb581902a5030a58b55
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-11-15 18:33:55 -0500

    outlines for p02 to p04

---
+++ 10_mini_projects/p03_RandomPasswordGenerator.py
@@ -0,0 +1,19 @@
+"""
+To create a program 
+that takes a number and 
+generate a random password length of that number.
+
+Topics: random module, joining strings, taking input
+
+Hint: Create a string with all characters, 
+then take random characters from it and 
+concatenate each char to make a big string.
+"""
+
+
+# import random
+# passlen = int(input("enter the length of password"))
+# s="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+# p =  "".join(random.sample(s,passlen ))
+# print (p)
+
